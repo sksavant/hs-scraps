26a8f896432dfa96e5642e12ac00e9b47b39375c
Savant Krishna <savant.2020@gmail.com>
2013-08-12 14:29:32 +0530

    more functions

--- vars.hs
+++ vars.hs
@@ -16,3 +16,15 @@ double x = 2*x
 quadruple x = double (double x) -- call a function in the definition of another
 square x = x*x
 half x = x/2
+
+-- Functions with multiple parameters
+area_of_rect l w = l*w
+area_tri_right b h = 1/2 * b * h
+-- Arguments are always passed in the order
+area_sq s = area_of_rect s s
+
+-- Local definitions
+heron a b c = sqrt (s*(s-a)*(s-b)*(s-c))
+    where
+    s = (a+b+c)/2
+-- Variables in function arguments have scope till the function only
