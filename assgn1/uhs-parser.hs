63f6cb92c62212ff1efd22e87552b7486479910a
Savant Krishna <savant.2020@gmail.com>
2013-09-09 21:53:36 -0400

    defined the data types, split the whole string into line by line list

--- assgn1/uhs-parser.hs
+++ assgn1/uhs-parser.hs
@@ -1,5 +1,23 @@
--- Parse x is the wrapper around parse which returns the parsed function
-parse x = x
+-- Author : S Krishna Savant <savant.2020@gmail.com>
+-- Parser for u-haskell : Major Assignment 1
+import Data.List
+
+type FName = String
+type Var = String
+
+data Program    = Prog [Fundef] Exp deriving Show
+data Fundef     = Fun String [String] Exp deriving Show
+data Exp        = I Int
+                | V Var
+                | B Bool
+                | Nil
+                | FName String
+                | App Exp Exp
+    deriving Show
+
+-- parse x is the wrapper
+-- First break with '\n' into a list of strings with lines
+parse x = lines x
 
 main = do 
     input <- readFile "pfile"
