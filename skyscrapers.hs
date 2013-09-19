937ac6c0ee5c6efa9ff921eca7317b9355693990
Savant Krishna <savant.2020@gmail.com>
2013-09-19 15:18:03 +0530

    skyscrapers puzzle start

---
+++ skyscrapers.hs
@@ -0,0 +1,10 @@
+-- Filename : skyscrapers.hs
+-- Author : Savant Krishna <savant.2020@gmail.com>
+-- Skyscrapers game solving using haskell
+
+data Board =   [['.','.','.','.'],
+                ['.','.','.','.'],
+                ['.','.','.','.'],
+                ['.','.','.','.'],
+                ['.','.','.','.']]
+
