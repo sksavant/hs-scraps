af9b31b7b04c4955262672f4bf068304fa0cea09
Savant Krishna <savant.2020@gmail.com>
2013-09-20 08:29:02 +0530

    prob 11 solved : facts are true :P

--- tutorials/list-tut.hs
+++ tutorials/list-tut.hs
@@ -78,4 +78,39 @@ cprod (x:[]) = [[xn] | xn <- x]
 cprod (x:xs) = [xn:yn | xn <- x, yn <- (cprod xs)]
 
 -- prob 9
+-- given 2 arrays of integers, find a subset of maximum elements which have the same relative order in both
+connect :: [Int] -> [Int] -> [Int]
+connect a b = longestlist (sameordersubsets a b)
+sameordersubsets a b = [ax | ax <- (subsets a), bx<- (subsets b), ax==bx]
+
+subsets :: [Int] -> [[Int]]
+subsets (x:[]) = [[],[x]]
+subsets (x:xs) = f x (subsets xs)
+    where   f x (y:ys) = (x:y):y:(f x ys)
+            f x [] = []
+
+-- prob 10 @TODO
+-- Pascal's triangle is a list of lists
+
+
+-- prob 11
+-- Puzzle : 2 numbers a and b from [2,99] are picked. S knows sum, P knows product
+good_nums = [2..99]::[Int]
+good_factors p = [(a,b)| a<-good_nums, b<-good_nums, a>=b, a*b==p]
+good_summands s = [(a,b)| a<-good_nums, b<-good_nums, a>=b, a+b==s]
+
+singleton :: [a] -> Bool
+singleton [] = False
+singleton (x:[]) = True
+singleton (x:xs) = False
+
+fact1 (a,b) = not (singleton (good_factors (a*b)))
+fact2 (a,b) = not (singleton (good_summands (a+b)))
+fact3 (a,b) = all fact1 [x|x <- (good_summands (a+b))])
+fact4 (a,b) = singleton (filter (==True) [fact3 x|x <- (good_factors (a*b))])
+fact5 (a,b) = singleton (filter (==True) [fact4 x|x <- (good_summands (a+b))])
+result = [(a,b) | a<-good_nums, b<-good_nums, (fact1 (a,b)) == True, (fact2 (a,b)) == True, (fact3 (a,b)) == True, (fact4 (a,b)) == True, (fact5 (a,b)) == True]
+-- Not Working. WHY? @TODO
+
+-- prob 12 @TODO
 --
