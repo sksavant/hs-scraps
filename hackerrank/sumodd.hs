0feae7532f9e5c47923033dc6a167b09940e094f
Savant Krishna <savant.2020@gmail.com>
2013-10-05 01:51:48 +0530

    add odd numbers in a list

---
+++ hackerrank/sumodd.hs
@@ -0,0 +1,18 @@
+--Find sum of all odd elements of a list
+f = sum . dropAll isEven -- Fill up this function body
+
+dropAll :: (a -> Bool) -> [a] -> [a]
+dropAll f x = g f x []
+    where   g f [] c = c
+            g f (x:xs) c = g f xs (h f x c)
+            h f x c = if (f x) then c else x:c
+
+
+isEven :: Int -> Bool
+isEven n    | (n`mod`2)==1 = False
+            | otherwise  = True
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+   inputdata <- getContents
+   putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
