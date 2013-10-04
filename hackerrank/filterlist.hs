50bb9f834176189412ee945b9f044ac6717dd0f5
Savant Krishna <savant.2020@gmail.com>
2013-10-05 01:39:06 +0530

    filtering list; have only elems which are lesser than some specified number

---
+++ hackerrank/filterlist.hs
@@ -0,0 +1,8 @@
+-- Filter the list to leave only numbers more than n
+f n = dropWhile (\x -> (x>=n)) --Fill up this function
+
+-- The Input/Output section. You do not need to change or modify this part
+main = do
+   n <- readLn :: IO Int
+   inputdata <- getContents
+   mapM_ putStrLn $ map show $ f n $ map (read :: String -> Int) $ lines inputdata
