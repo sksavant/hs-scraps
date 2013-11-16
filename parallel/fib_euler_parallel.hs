7d469dadeb6942c83d2baaaefa69ddbda229734f
Savant Krishna <savant.2020@gmail.com>
2013-11-17 01:41:14 +0530

    some parallel haskell

---
+++ parallel/fib_euler_parallel.hs
@@ -0,0 +1,42 @@
+import Control.Parallel
+import System.Time
+
+secDiff :: ClockTime -> ClockTime -> Float
+secDiff (TOD secs1 psecs1) (TOD secs2 psecs2) = fromInteger(psecs2 - psecs1)/1e12 + fromInteger(secs2-secs1)
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+mkList :: Int -> [Int]
+mkList n = [1..n-1]
+
+relprime :: Int -> Int -> Bool
+relprime x y = gcd x y == 1
+
+euler :: Int -> Int
+euler n = length ( filter (relprime n) (mkList n))
+
+sumEuler :: Int -> Int
+sumEuler = sum . (map euler) . mkList
+
+sumFibEuler :: Int -> Int -> Int
+sumFibEuler a b = fib a + sumEuler b
+
+parSumFibEuler :: Int -> Int -> Int
+parSumFibEuler a b = f `par` (e `pseq` (e+f))
+    where   f = fib a
+            e = sumEuler b
+
+r1 :: Int
+--r1 = sumFibEuler 38 5300
+r1 = parSumFibEuler 38 5300
+
+main::IO()
+main = do
+    t0 <- getClockTime
+    pseq r1 (return())
+    t1 <- getClockTime
+    putStrLn ("sum:" ++ show r1)
+    putStrLn ("time:" ++ show (secDiff t0 t1))
